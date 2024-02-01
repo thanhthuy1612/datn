8dee6a04985b61e5375fa7624ab6f7569a39ba0d
latiah.bui <latiah.bui@avepoint.com>
2024-02-01 14:49:49 +0700

    update 01/02/2024

--- fe/src/pages/home/Carousel.tsx
+++ fe/src/pages/home/Carousel.tsx
@@ -1,31 +1,58 @@
 import React from "react";
 import { Carousel } from "antd";
-import carousel1 from "../../assets/carousel1.png"
+import { CiDeliveryTruck, CiShop, CiShoppingBasket, CiShuffle, CiWheat } from "react-icons/ci";
+import carousel1 from "../../assets/carousel1.jpg"
 import carousel2 from "../../assets/carousel2.png"
-import carousel3 from "../../assets/carousel3.png"
-import carousel4 from "../../assets/carousel4.jpg"
 
 interface ICarousel {
   id: number;
-  banner: string;
+  banner: React.ReactNode;
 }
+const renderCarosel1 = () => (
+  <div className="h-[100%] flex justify-center items-center relative">
+    <img alt="1" src={carousel1} className="w-[100%] h-auto" />
+    <p className="absolute flex justify-center top-[150px] py-[40px] text-[50px] bg-[rgba(255,255,255,0.7)] w-[100%] font-[700]">TRUY XUẤT NGUỒN GỐC SẢN PHẨM</p>
+  </div>
+)
+
+const renderCarosel2 = () => (
+  <div className="h-[100%] flex justify-center items-center relative">
+    <img alt="1" src={carousel2} className="w-[100%] h-auto" />
+    <div className="absolute bg-[rgba(0,0,0,0.7)] py-[40px] top-[60px] w-[100%]">
+      <div className="flex justify-around items-center">
+        <div className="w-[300px] flex flex-col items-center">
+          <CiWheat className="h-[80px] w-[80px] text-white" />
+          <p className="text-[30px] text-white">Nhà sản xuất</p>
+        </div>
+        <CiShuffle className="h-[80px] w-[80px] text-white" />
+        <div className="w-[300px] flex flex-col items-center">
+          <CiDeliveryTruck className="h-[80px] w-[80px] text-white" />
+          <p className="text-[30px] text-white">Vận chuyển</p>
+        </div>
+        <CiShuffle className="h-[80px] w-[80px] text-white" />
+        <div className="w-[300px] flex flex-col items-center">
+          <CiShop className="h-[80px] w-[80px] text-white" />
+          <p className="text-[30px] text-white">Đại lý</p>
+        </div>
+        <CiShuffle className="h-[80px] w-[80px] text-white" />
+        <div className="w-[300px] flex flex-col items-center">
+          <CiShoppingBasket className="h-[80px] w-[80px] text-white" />
+          <p className="text-[30px] text-white">Người tiêu dùng</p>
+        </div>
+      </div>
+      <p className="text-[30px] text-white mt-[50px] flex w-[100%] justify-center border-t-white border-t-[1px] pt-[30px]">MÔ HÌNH HỆ THỐNG TRUY XUẤT NGUỒN GỐC SẢN PHẨM</p>
+    </div>
+  </div>
+)
 
 const carousel: ICarousel[] = [
   {
     id: 1,
-    banner: carousel1,
+    banner: renderCarosel1(),
   },
   {
     id: 2,
-    banner: carousel2,
-  },
-  {
-    id: 3,
-    banner: carousel3,
-  },
-  {
-    id: 4,
-    banner: carousel4,
+    banner: renderCarosel2(),
   },
 ];
 
@@ -36,13 +63,7 @@ const CarouselHome: React.FC = () => (
         <div
           className="w-[100%] h-[450px] bg-hover rounded-[20px] shadow-2xl border-border border-[1px]"
           key={item.id}>
-          <div className="h-[100%] flex justify-center items-center">
-          <img
-            src={item.banner}
-            className="w-[100%] h-auto"
-            key={item.id}
-          />
-          </div>
+          {item.banner}
         </div>
       ))}
     </Carousel>
